6626ec9d5462d58793cfbba5f474460198c0b059
Anvith Alva <anvithalva02@gmail.com>
2025-06-11 12:42:06 +0530

    Implement dynamic redirection based on short URL from MongoDB

---
+++ app/[shorturl]/page.js
@@ -0,0 +1,22 @@
+import clientPromise from "@/lib/mongodb"
+import { redirect } from "next/navigation"
+
+export default async function Page({ params }) {
+    const shorturl = (await params).shorturl
+
+
+    const client = await clientPromise
+    const db = client.db("BitLink")
+    const collection = db.collection("urls")
+
+
+    const doc = await collection.findOne({
+        shorturl: shorturl
+    })
+    if (doc) {
+        redirect(doc.url)
+    }
+    else {
+        redirect(process.env.NEXT_PUBLIC_HOST)
+    }
+}
